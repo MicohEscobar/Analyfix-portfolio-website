7e5a291db6cadf489f2e556d4e41cc7e3cac4511
MicohEscobar <mayakadzanja@gmail.com>
2023-07-30 19:14:26 +0200

    added dropdown lists on Header

--- analyfix/header.php
+++ analyfix/header.php
@@ -1,76 +1,139 @@
-<style>
-  header {
-    text-align: center;
-    padding: 20px;
-  }
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    header {
+      text-align: center;
+      padding: 20px;
+    }
 
-  .header-nav {
-    display: inline-block;
-    margin-bottom: 20px;
-  }
+    .header-nav {
+      display: inline-block;
+      margin-bottom: 20px;
+    }
 
-  .header-nav ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
+    .header-nav ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    }
 
-  .header-nav li {
-    display: inline-block;
-    margin-right: 20px;
-  }
+    .header-nav li {
+      display: inline-block;
+      margin-right: 20px;
+      position: relative; /* Add this to make the dropdown work */
+    }
 
-  .header-nav li:last-child {
-    margin-right: 0;
-  }
+    .header-nav li:last-child {
+      margin-right: 0;
+    }
 
-  .header-nav a {
-    text-decoration: none;
-    color: #333;
-    font-weight: bold;
-    padding: 5px 10px;
-    border: 2px solid #333;
-    border-radius: 5px;
-  }
+    .header-nav a {
+      text-decoration: none;
+      color: #333;
+      font-weight: bold;
+      padding: 5px 10px;
+      border: 2px solid #333;
+      border-radius: 5px;
+    }
 
-  .header-nav a:hover {
-    background-color: #333;
-    color: #fff;
-  }
+    .header-nav a:hover {
+      background-color: #333;
+      color: #fff;
+    }
 
-  .dashboard-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-  }
+    /* Dropdown styles */
+    .header-nav ul.dropdown-content {
+      display: none;
+      position: absolute;
+      top: 100%;
+      left: 0;
+      background-color: #f9f9f9;
+      border: 1px solid #ccc;
+      padding: 0;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Add box shadow for a subtle effect */
+    }
 
-  .dashboard-info {
-    flex: 1;
-    text-align: center;
-  }
+    .header-nav li:hover ul.dropdown-content {
+      display: block;
+    }
 
-  .dashboard-image {
-    flex: 1;
-    text-align: center;
-  }
-</style>
+    .header-nav li.dropdown li {
+      display: block;
+    }
 
-<header>
-  <nav class="header-nav">
-    <ul>
-      <li><a href="index.php">Home</a></li>
-      <li><a href="services.php">Services</a></li>
-      <li><a href="#about">About Us</a></li>
-    </ul>
-  </nav>
-  <div class="dashboard-container">
-    <div class="dashboard-info">
-      <h1>AnalyFix</h1>
-      <p>Empowering businesses with data-driven solutions</p>
+    .header-nav li.dropdown li a {
+      padding: 10px;
+      color: #333;
+      text-decoration: none;
+      display: block;
+    }
+
+    .header-nav li.dropdown li a:hover {
+      background-color: #f2f2f2;
+    }
+
+    /* Additional styles for better appearance */
+    .header-nav li.dropdown:hover > a {
+      background-color: #333; /* Change the background color of the main link on hover */
+      color: #fff;
+    }
+
+    .header-nav li.dropdown li:last-child a {
+      border-bottom-left-radius: 5px; /* Round the bottom left corner of the last item in the dropdown */
+      border-bottom-right-radius: 5px; /* Round the bottom right corner of the last item in the dropdown */
+    }
+
+    .dashboard-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-wrap: wrap;
+    }
+
+    .dashboard-info {
+      flex: 1;
+      text-align: center;
+    }
+
+    .dashboard-image {
+      flex: 1;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <nav class="header-nav">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li class="dropdown">
+          <a href="services.php">Services</a>
+          <ul class="dropdown-content">
+            <li><a href="#">Service 1</a></li>
+            <li><a href="#">Service 2</a></li>
+            <li><a href="#">Service 3</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#about">About Us</a>
+          <ul class="dropdown-content">
+            <li><a href="#">Our Story</a></li>
+            <li><a href="#">Our Team</a></li>
+            <li><a href="#">Mission and Vision</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <div class="dashboard-container">
+      <div class="dashboard-info">
+        <h1>AnalyFix</h1>
+        <p>Empowering businesses with data-driven solutions</p>
+      </div>
+      <div class="dashboard-image">
+        <img src="plc.jpg" alt="Dashboard Image">
+      </div>
     </div>
-    <div class="dashboard-image">
-      <img src="plc.jpg" alt="Dashboard Image">
-    </div>
-  </div>
-</header>
+  </header>
+</body>
+</html>
--- analyfix/index.php
+++ analyfix/index.php
@@ -10,23 +10,7 @@
 <body>
   
 
-  <section class="services">
-    <h1>What we offer</h1>
-    <div class="service-item">
-      <h3>Data Analysis and Visualization</h3>
-      <p>Extracting valuable insights from data and presenting them visually.</p>
-    </div>
-    <div class="service-item">
-      <h3>Predictive Modeling</h3>
-      <p>Using machine learning to forecast future trends and make predictions.</p>
-    </div>
-    <div class="service-item">
-      <h3>Data Cleaning and Preprocessing</h3>
-      <p>Ensuring data quality and integrity for accurate analysis.</p>
-    </div>
-    <div class="service-item">
-      <h3>Big Data Analytics</h3>
-      <p>Analyzing large-scale datasets for actionable insights.</p>
+  
     </div>
     <a class="cta-btn" href="#contact">Contact Us</a>
   </section>
