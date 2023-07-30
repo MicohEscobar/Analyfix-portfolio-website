cf2edba4a1dc07d1fe0e71f6d10f97a6eed716f5
MicohEscobar <mayakadzanja@gmail.com>
2023-07-30 21:50:35 +0200

    Modified services.php

--- analyfix/services.php
+++ analyfix/services.php
@@ -28,16 +28,40 @@
         <a href="predictive_modelling.php">
             <h3>Predictive Modeling</h3>
         </a>
-            <p>Using machine learning to forecast future trends and make predictions.</p>
+            <p>Using machine learning to forecast future trends and make predictions.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
+minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
+fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
+consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+doloremque. Quaerat provident commodi consectetur veniam similique ad 
+earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
+fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
+suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
+totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
+quasi aliquam eligendi, placeat qui corporis!
+            </p>
+            
         </div>
         
         <div class="service-item">
-        <a href= "data_cleaning.php">
+        <a href= "data_cleaning">
             <h3>Data Cleaning and Preprocessing</h3>
         </a>
+        
             <p>Ensuring data quality and integrity for accurate analysis.</p>
         </div>
-        <div class="service-item">
+        <div class="service-item">  
         <a href=big_data.php>
             <h3>Big Data Analytics</h3>
         </a>
